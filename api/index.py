c5777b323709c60a5215056e9089da889d2ad344
Cenentury0941 <67197834+cenentury0941@users.noreply.github.com>
2023-07-28 13:03:07 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,24 @@
 from http.server import BaseHTTPRequestHandler
 
+import requests
+from requests.auth import HTTPDigestAuth
+
+
+
+
+
+
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+
+	response = requests.get(
+    	'https://eu-central-1.data.tidbcloud.com/api/v1beta/app/dataapp-BSWBoAYM/endpoint/sandbox_state',
+    	auth=HTTPDigestAuth('jN3cMFav', '2b229f3c-baa2-4afa-9fb8-337e3b211c1e'),
+	)
+
+        self.wfile.write(response.text.encode('utf-8'))
         return
